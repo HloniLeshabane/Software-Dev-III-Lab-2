1fc000b6f3fe110e830bfbbd559e4a818f4243b1
HloniLeshabane <99leshabane@gmail.com>
2021-04-20 20:30:26 +0200

    images

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+var path = require('path');
 let port = process.env.PORT || 3000;
 
 app.set('view engine','ejs');
+app.use(Express.static(__dirname+'/public'));
 
 app.get('/',(req,res)=>{
     res.render('index');
