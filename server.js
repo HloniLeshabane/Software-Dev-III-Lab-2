e40dc91e365af2ab1ac2897e82e764a523329d89
HloniLeshabane <99leshabane@gmail.com>
2021-04-20 17:17:12 +0200

    Port for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+let port = process.env.PORT || 3000;
 
 app.set('view engine','ejs');
 
@@ -7,4 +8,4 @@ app.get('/',(req,res)=>{
     res.render('index');
 })
 
-app.listen(3000);
+app.listen(port);
